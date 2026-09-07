f6a2a65b72a654fce2fce186f8664851357fb09b
liang-senbei <FranccescoNal.da@gmail.com>
2026-09-07 09:49:00 +0000

    En：开 / 关

--- android/app/src/main/kotlin/app/yxi/ui/En.kt
+++ android/app/src/main/kotlin/app/yxi/ui/En.kt
@@ -1536,6 +1536,7 @@ internal object En {
         "狂热" to "Frenzy",
         "癫狂" to "Wild",
         "闪屏 / 抖动" to "Flash / shake",
+        "开" to "On",
         "娱乐中心" to "Games",
         "玩一会儿。成绩记在账号上，奖励照发。" to "Take a break. Scores go to your account, rewards still drop.",
         "下载这首" to "Download",
